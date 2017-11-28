6a8fb44b38ef8df13a03a84ec9cf9efa5a668b2a
lnielsen <louiseenielsen@gmail.com>
2017-11-28 10:17:52 -0500

    working in header file 5 sensor code

---
+++ Sprint2Integrate/Sensor.h
@@ -0,0 +1,14 @@
+/*
+ * Sensor.h - Use 5 VL53L0X sensors
+ */
+#ifndef Sensor_h
+#define Sensor_h
+
+#include <Arduino.h>
+#include <Wire.h>
+#include <VL53L0X.h>
+
+void sensorSetup();
+void sensorRead();
+
+#endif
